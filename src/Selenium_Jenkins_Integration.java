5f3907b2fa45114d65a12df8f23fdc089821dcfa
Cybertek Student <putbol10@gmail.com>
2018-02-18 16:11:50 -0500

    first push

---
+++ src/Selenium_Jenkins_Integration.java
@@ -0,0 +1,13 @@
+
+public class Selenium_Jenkins_Integration {
+
+	public static void main(String[] args) {
+		
+	
+		System.setProperty("webdriver.chrome.driver", "C:\Selenium_libs\chromedriver.exe");	
+	
+	WebDriver driver = new ChromeDriver();
+	
+	}
+
+}
